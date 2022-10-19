02395c5ef2814fe78fec5f754bc03218f713c4c7
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-10-19 17:58:59 +0600

    google popup method working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from "./Firebase/Firebase.config";
+
+const auth = getAuth(app);
 
 function App() {
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .error((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>app.js file</h1>
+      <button onClick={handleGoogle}>google coming </button>
     </div>
   );
 }
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZRWHswnkN7O6iApSBTeZqWR7KDAH2oDw",
+  authDomain: "fir-first-project-6a0ca.firebaseapp.com",
+  projectId: "fir-first-project-6a0ca",
+  storageBucket: "fir-first-project-6a0ca.appspot.com",
+  messagingSenderId: "627008122447",
+  appId: "1:627008122447:web:59f120b4d57ee65b57443a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
